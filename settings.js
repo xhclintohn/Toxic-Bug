8fc11a65eb75b7d385491f622e794e241eddb344
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2024-07-27 14:17:06 +0300

    ./

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.location = "Africa, Kenya, Nairobi"
 
 global.ownernumber = '254735342808,254799283147,254114374044'  //creator number
 global.ownername = 'xhclinton' //owner name
-global.botname = '🕷Venom-v2🕷' //name of the bot
+global.botname = '☣️Toxic -1☣️' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
